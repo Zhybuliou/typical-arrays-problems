17011ea468d4bc6555e13ef1f5ed2cf807d249c4
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-09-30 00:18:05 +0300

    docs: array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array)){
+  return Math.min.apply(null, array);}
+  else{return 0;}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(Array.isArray(array)){
+        return Math.max.apply(null, array);}
+        else{return 0;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(Array.isArray(array)){
+        var count = 0;
+        for(let i = 0; i < array.length; i++) {
+        count += array[i];
+        }
+        return count / array.length;
+    }
+        else{return 0;}
 }
