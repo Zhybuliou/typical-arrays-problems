39833f60e072bcb59f6840f9901538490fcaf043
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-09-30 00:27:29 +0300

    docs: done task array problem

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 
 exports.min = function min (array) {
-  if(Array.isArray(array)){
+  if(typeof array !== 'undefined' && array.length > 0){
   return Math.min.apply(null, array);}
   else{return 0;}
 }
 
 exports.max = function max (array) {
-    if(Array.isArray(array)){
+    if(typeof array !== 'undefined' && array.length > 0){
         return Math.max.apply(null, array);}
         else{return 0;}
 }
 
 exports.avg = function avg (array) {
-    if(Array.isArray(array)){
+    if(typeof array !== 'undefined' && array.length > 0){
         var count = 0;
         for(let i = 0; i < array.length; i++) {
         count += array[i];
